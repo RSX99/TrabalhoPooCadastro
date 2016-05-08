72f06c672ae42806c7e1bee783876991bb7f4589
Robson Xavier <dyn.ba@hotmail.com>
2016-05-08 19:23:12 -0300

    Added files via upload

---
+++ MainClass.java
@@ -0,0 +1,17 @@
+package MainApp;
+import Menu.HomeScreen;
+
+
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HomeScreen hs = new HomeScreen();
+	
+		hs.Home();
+
+	}
+
+}
